3eb497cc3511d6ba6bf9a88ec63563ba0eda2057
akshayrampuria <rampuria@stanford.edu>
2016-11-26 21:12:18 -0800

    Everything functionally works. Cleaning up required

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request
+import requests, json
 
 app = Flask(__name__)
+HOST = 'http://api.giphy.com'
+PATH = '/v1/gifs/search'
+API_KEY = 'dc6zaTOxFJmzC'
 
 @app.route('/')
 def search():
@@ -8,7 +12,25 @@ def search():
 
 @app.route('/results', methods = ['POST'])
 def results():
-	return "GIF to search:" + request.form['query']
+	
+	params = {}
+	
+	params['q'] = request.form['query']
+	params['fmt'] = 'json'
+	params['api_key'] = API_KEY
+
+	r = requests.get(HOST + PATH, params=params)
+	gif_urls = []
+	if r.text == ' \n\n':
+		return "Sorry, no GIF's were found."
+	else:
+		data = json.loads(r.text)['data']
+		for url in data:
+			gif_urls.append(url['images']['fixed_width']['url'])
+
+	print gif_urls
+	return render_template('results.html', data=gif_urls)	
+	
 
 app.debug = True	
 app.run()
