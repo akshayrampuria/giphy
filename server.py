1fadf616417eff100dfd7abc9d5ac28988d33225
akshayrampuria <rampuria@stanford.edu>
2016-11-26 20:34:37 -0800

    Search page works

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def search():
-	return "Hi, I work! YAY!"
+	return render_template('search.html')
+
+@app.route('/results', methods = ['POST'])
+def results():
+	return "GIF to search:" + request.form['query']
 
 app.debug = True	
 app.run()
